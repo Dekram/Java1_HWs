9cb5f154060d7184d443e14b5ad6f570a615d13e
Dekram <madxtreme@gmail.com>
2017-05-04 01:14:07 +0300

    first homework
    light

---
+++ Java1_HW1/src/Dekram/Task_1.java
@@ -0,0 +1,21 @@
+﻿package Dekram;
+
+
+/**
+ * Created by Dekram on 15.02.2017.
+ */
+public class Task_1 {
+    public static void main(String[] args) {
+
+        byte b = 16;
+        short s = 2;
+        int i = 2017;
+        long l = 30L;
+        float f = 11f;
+        double d = 1984d;
+        boolean b1 = true;
+        char v = 'V';
+        char m = 'M';
+
+    }
+}
